092a6856a15ef47a647bd49f6c19e470fe5a6803
Perflyst <mail@perflyst.de>
2018-03-27 21:29:45 +0200

    Add fbcdn.net

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 const FACEBOOK_CONTAINER_NAME = "Facebook";
 const FACEBOOK_CONTAINER_COLOR = "blue";
 const FACEBOOK_CONTAINER_ICON = "briefcase";
-const FACEBOOK_DOMAINS = ["facebook.com", "www.facebook.com", "fb.com", "instagram.com", "www.instagram.com", "whatsapp.com", "www.whatsapp.com", "web.whatsapp.com"];
+const FACEBOOK_DOMAINS = ["facebook.com", "www.facebook.com", "fb.com", "fbcdn.net", "instagram.com", "www.instagram.com", "whatsapp.com", "www.whatsapp.com", "web.whatsapp.com"];
 
 const MAC_ADDON_ID = "@testpilot-containers";
 
