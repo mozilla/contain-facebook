a55ead1c921027bbd1e0ea1303bc26bab1b813a6
stoically <stoically@protonmail.com>
2018-03-30 07:50:28 +0200

    Add feature tests

---
+++ test/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
+    "env": {
+        "node": true,
+        "mocha": true
+    },
+    "globals": {
+        "expect": false,
+        "loadWebExtension": false,
+        "sinon": false
+    }
+};
