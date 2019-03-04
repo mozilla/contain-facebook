ee2649168afeebb4f06e214cdb5815eaece0683b
Shivangi Kakkar <skakkar@skakkar-l5jgh5.local>
2019-03-05 02:50:17 +0530

    typo changes

--- src/panel.js
+++ src/panel.js
@@ -1,1 +1,18 @@
+var firstPanel = document.getElementById("#panel-1");
+var secondPanel = document.getElementById("#panel-2");
 
+
+
+function panel1(){
+    secondPanel.display= "none";
+}
+
+function panel2() {
+    secondPanel.show;
+}
+
+function showPanel() {
+    browser.browserAction.onClicked(
+        secondPanel
+    )
+}
