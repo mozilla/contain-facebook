d297c3257b01995cad89367291beca407a1eefc4
Maxx Crawford <maxx.crawford@gmail.com>
2019-04-11 10:42:59 -0500

    Added replacement query/content for FB login

--- src/content_script.js
+++ src/content_script.js
@@ -1,6 +1,24 @@
 "use strict";
 
+function detectFacebookLoginButton() {
+  // Test for standard implementation (Example: Facebook Docs)
+  console.log("detectFacebookLoginButton");
+  const loginButton = document.querySelectorAll(".fb-login-button");
+  for (let item of loginButton) {
+    let replacementClassArr = ["fbc-loginButton", "fbc-overlay"];
+    replacementClassArr.push("fbc-size-" + item.getAttribute("data-size"));
+    replacementClassArr.push("fbc-button-type-" + item.getAttribute("data-button-type"));
+    item.classList.add(...replacementClassArr);
+  }
+}
+
 browser.runtime.onMessage.addListener(message => {
   console.log("message from background script:", message);
+  setTimeout(function(){
+    detectFacebookLoginButton();
+  }, 10);
+
   return Promise.resolve({response: "content_script onMessage listener"});
 });
+
+setTimeout(detectFacebookLoginButton, 150);
