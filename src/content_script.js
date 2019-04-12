369caf541e51ed7a7c9a5a3d71ccecb9b3694162
Maxx Crawford <maxx.crawford@gmail.com>
2019-04-12 11:41:12 -0500

    Restyled badge, added size detection in JS

--- src/content_script.js
+++ src/content_script.js
@@ -1,32 +1,83 @@
 "use strict";
 
+function itemWidthCheck(target){
+
+  const itemHeight = target.offsetHeight;
+  const itemWidth = target.offsetWidth;
+  let iconClassArr = ["fbc-overlay"];
+
+  const ratioCheck = ( itemWidth / itemHeight );
+  console.log("itemWidthCheck", ratioCheck);
+
+  if ( ratioCheck < 1.1 ) {
+    iconClassArr.push("fbc-overlay-small");
+  } else if ( itemHeight < 39 ) {
+    iconClassArr.push("fbc-overlay-small");
+  }
+  console.log(iconClassArr);
+  return iconClassArr;
+}
+
 function detectFacebookOnPage () {
 
   // TODO: Loop through array of class names to test for
   // Test for any element with facebook connect in class name (Example: Pinterest)
+
+  const loginButtonTitle = document.querySelectorAll("[title='Share On Facebook']");
+  console.log(loginButtonTitle);
+  for (let item of loginButtonTitle) {
+    console.log("loginButtonTitle", item);
+    if ( !item.classList.contains("fbc-overlay") ) {
+      item.classList.add(...itemWidthCheck(item));
+    }
+
+  }
+
   const loginButtonClass = document.querySelectorAll("[class*='FacebookConnectButton']");
   for (let item of loginButtonClass) {
-    item.classList.add("fbc-overlay");
+
+    console.log("loginButtonClass", item);
+    if ( !item.classList.contains("fbc-overlay") ) {
+      item.classList.add(itemWidthCheck(item));
+    }
+    // item.classList.add("fbc-overlay", itemWidthCheck(item));
   }
 
   // Test for anchor/facebook URL (Example: Imgur)
   const loginButtonHref = document.querySelectorAll("[href*='facebook']");
   for (let item of loginButtonHref) {
-    item.classList.add("fbc-overlay");
+    console.log("loginButtonHref", item);
+    if ( !item.classList.contains("fbc-overlay") ) {
+      item.classList.add(itemWidthCheck(item));
+    }
+    // item.classList.add("fbc-overlay", itemWidthCheck(item));
+  }
+  const likeButtonBfaNetwork = document.querySelectorAll("['data-bfa-network'*='facebook']");
+  for (let item of likeButtonBfaNetwork) {
+    console.log("likeButtonBfaNetwork", item);
+    if ( !item.classList.contains("fbc-overlay") ) {
+      item.classList.add(itemWidthCheck(item));
+    }
+    // item.classList.add("fbc-overlay", itemWidthCheck(item));
   }
 
   const loginButtonDataOAuth = document.querySelectorAll("[data-oauthserver*='facebook']");
   for (let item of loginButtonDataOAuth) {
-    item.classList.add("fbc-overlay");
+    console.log("loginButtonDataOAuth", item);
+    if ( !item.classList.contains("fbc-overlay") ) {
+      item.classList.add(itemWidthCheck(item));
+    }
+    // item.classList.add("fbc-overlay", itemWidthCheck(item));
   }
 
-  const loginButtonTitle = document.querySelectorAll("[title*='Share On Facebook']");
-  for (let item of loginButtonTitle) {
-    item.classList.add("fbc-overlay");
-  }
+
   const loginButtonAriaLabel = document.querySelectorAll("[aria-label*='Facebook']");
   for (let item of loginButtonAriaLabel) {
-    item.classList.add("fbc-overlay");
+    console.log("loginButtonAriaLabel", item);
+    if ( !item.classList.contains("fbc-overlay") ) {
+      item.classList.add(itemWidthCheck(item));
+    }
+    // item.classList.add("fbc-overlay", itemWidthCheck(item));
   }
 }
 
