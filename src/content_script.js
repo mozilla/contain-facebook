f83e05d2e7e1207affcf24534748c67be343f0ce
Maxx Crawford <maxx.crawford@gmail.com>
2019-04-19 11:19:51 -0500

    Added fixed position detection support

--- src/content_script.js
+++ src/content_script.js
@@ -34,14 +34,23 @@ function itemWidthCheck (target) {
 
 let htmlBadgeDiv;
 
+function isFixed (elem) {
+  do {
+    if (getComputedStyle(elem).position == "fixed") return true;
+  } while ( (elem = elem.offsetParent) );
+  return false;
+}
+
+
 function addFacebookBadge(target, badgeClassUId){
   htmlBadgeDiv = document.createElement('div');
   htmlBadgeDiv.className = "fbc-badge " + badgeClassUId;
-  // htmlBlock.dataset.selector = item;
   document.body.appendChild(htmlBadgeDiv);
 
   const itemWidth = parseInt(target.offsetWidth,10);
   const itemHeight = parseInt(target.offsetHeight,10);
+
+  console.log("isFixed: ", isFixed(target) );
 
   const ratioCheck = (itemWidth / itemHeight);
   let badgeSmallSwitch = false;
@@ -52,59 +61,68 @@ function addFacebookBadge(target, badgeClassUId){
   } else if (itemHeight < 39) {
     htmlBadgeDiv.classList.add("fbc-badge-small");
     badgeSmallSwitch = true;
-}
+  }
 
   positionFacebookBadge(target, badgeClassUId, itemWidth, badgeSmallSwitch);
 }
 
 function positionFacebookBadge( target, badgeClassUId, targetWidth, smallSwitch ) {
 
-    console.log(typeof target);
-    console.log(target.constructor);
+  // Check for Badge element and select it
+  if (!badgeClassUId) {
+    badgeClassUId = "js-" + target;
+  }
 
-    if (!badgeClassUId) {
-      badgeClassUId = "js-" + target;
-      console.log("badgeClassUId: ", badgeClassUId);
-    } else {
-      console.log("badgeClassUId-Else: ", badgeClassUId);
+  htmlBadgeDiv = document.querySelector("." + badgeClassUId);
+
+  // Confirm target element is defined
+  if ( target && typeof target === "object" ) {
+    // TODO: Reverse IF Statement
+  } else {
+    target = document.querySelector("." + target);
+  }
+
+  // Set offset size based on large/small badge
+  let elementSizeOffsetX = 20;
+  let elementSizeOffsetY = 4;
+
+  if ( typeof smallSwitch == "undefined" ) {
+    if ( htmlBadgeDiv.classList.contains("fbc-badge-small") ) {
+      smallSwitch = true;
     }
+  }
 
-    if ( target && typeof target === 'object' ) {
-      console.log('objectTest-true');
-    } else {
-      console.log('objectElse');
-      target = document.querySelector("." + target);
-    }
+  if (smallSwitch) {
+    elementSizeOffsetX = 12;
+    elementSizeOffsetY = 5;
+  }
 
-    console.log("target: ", target);
-    // console.log("positionFacebookBadge: ",  target);
+  // Define target element width
+  if (!targetWidth) {
+    targetWidth = parseInt(target.offsetWidth,10);
+  }
 
+  // Get position coordinates
+  const bodyRect = document.body.getBoundingClientRect();
+  const elemRect = target.getBoundingClientRect();
 
+  let offsetPosX = elemRect.left - bodyRect.left;
+  let offsetPosY = elemRect.top - bodyRect.top;
 
-    if (!targetWidth) {
-      targetWidth = parseInt(target.offsetWidth,10);
-      console.log("tagetWidth: ", targetWidth);
-    }
+  if ( isFixed(target) ) {
+    htmlBadgeDiv.classList.add("fbc-badge-fixed");
+    offsetPosX = elemRect.left;
+    offsetPosY = elemRect.top;
+  } else {
+    htmlBadgeDiv.classList.remove("fbc-badge-fixed");
+  }
 
-    console.log(badgeClassUId);
-    htmlBadgeDiv = document.querySelector("." + badgeClassUId);
-    console.log("htmlBadgeDiv: ", htmlBadgeDiv);
+  const htmlBadgeDivPosX = (offsetPosX + targetWidth) - elementSizeOffsetX;
+  const htmlBadgeDivPosY = offsetPosY - elementSizeOffsetY;
 
-    const bodyRect = document.body.getBoundingClientRect();
-    const elemRect = target.getBoundingClientRect();
-    // const itemPseudoStyle = window.getComputedStyle(target, ':after');
-    // console.log(itemPseudoStyle);
-    let offsetPosX   = elemRect.left - bodyRect.left;
-    let offsetPosY   = elemRect.top - bodyRect.top;
-
-    const htmlBadgeDivPosX = (offsetPosX + targetWidth) - 20;
-    const htmlBadgeDivPosY = offsetPosY - 4;
-
-    console.log("XY POS: ", htmlBadgeDivPosX, htmlBadgeDivPosY);
-
-    // htmlBadgeDiv.style.cssText("left: " + htmlBadgeDivPosX + "px; top: " + htmlBadgeDivPosY + "px;" );
-    htmlBadgeDiv.style.left = htmlBadgeDivPosX + "px";
-    htmlBadgeDiv.style.top = htmlBadgeDivPosY + "px";
+  // Set badge position based on target coordinates/size
+  htmlBadgeDiv.style.left = htmlBadgeDivPosX + "px";
+  htmlBadgeDiv.style.top = htmlBadgeDivPosY + "px";
 
 }
 
@@ -206,22 +224,49 @@ function addToolTipBlock(item) {
 
 }
 
-
+// Resize listener. Only fires after window stops resizing.
 let resizeId;
 
-window.addEventListener('resize', function() {
-    clearTimeout(resizeId);
-    resizeId = setTimeout(doneResizing, 10);
+window.addEventListener("resize", function() {
+  clearTimeout(resizeId);
+  resizeId = setTimeout(doneResizing, 25);
 });
 
 function doneResizing(){
   for (let item of facebookDetectedElementsArr) {
-    console.log(item);
     positionFacebookBadge(item);
   }
-  console.log('doneResizing');
-
 }
+
+// On Scroll, checking for position fixed on elements
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doneScrolling() {
+  for (let item of facebookDetectedElementsArr) {
+    positionFacebookBadge(item);
+    // console.log("scrollCheckInit");
+    // let badgeClassUId = "js-" + item;
+    // htmlBadgeDiv = document.querySelector("." + badgeClassUId);
+    // if ( htmlBadgeDiv.classList.contains("fbc-badge-fixed") ) {
+    //   positionFacebookBadge(item);
+    // }
+  }
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doneScrolling(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
 
 browser.runtime.onMessage.addListener(message => {
   console.log("message from background script:", message);
