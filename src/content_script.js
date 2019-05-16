277786c54720f491e82968d85749c1e13631aa7a
Maxx Crawford <maxx.crawford@gmail.com>
2019-05-16 11:07:16 -0500

    Fixed #340, Added premierleague.com support

--- src/content_script.js
+++ src/content_script.js
@@ -15,6 +15,7 @@ const LOGIN_PATTERN_DETECTION_SELECTORS = [
   "[href*='facebook.com/v2.3/dialog/oauth']", // Spotify
   "[href*='/sign_in/Facebook']", // bazqux.com
   "[href*='signin/facebook']",
+  "[href*='accounts/facebook/login']", // premierleague.com
   "[data-oauthserver*='facebook']", // Stackoverflow
   "[id*='facebook_connect_button']", // Quora
   "[data-action*='facebook-auth']", //Medium
