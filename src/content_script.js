330b9cd0b7d30b36ad5654e1c7203d08ca4bb1b9
Miko Mynttinen <mikokm@gmail.com>
2019-10-23 16:24:52 +0200

    Join the selectors and call querySelectorAll only once

--- src/content_script.js
+++ src/content_script.js
@@ -473,20 +473,20 @@ const facebookDetectedElementsArr = [];
 
 function patternDetection(selectionArray, socialActionIntent){
   // console.log("patternDetection");
-  for (let querySelector of selectionArray) {
-    for (let item of document.querySelectorAll(querySelector)) {
-      // overlay the FBC icon badge on the item
-      if (!item.classList.contains("fbc-has-badge") && !isPinterest(item) && !parentIsBadged(item)) {
-        // console.log([querySelector, item]);
-        const itemUIDClassName = "fbc-UID_" + (facebookDetectedElementsArr.length + 1);
-        const itemUIDClassTarget = "js-" + itemUIDClassName;
-        const socialAction = socialActionIntent;
-        facebookDetectedElementsArr.push(itemUIDClassName);
-        addFacebookBadge(item, itemUIDClassTarget, socialAction);
-        item.classList.add("fbc-has-badge");
-        item.classList.add(itemUIDClassName);
-        // console.log(itemUIDClassName);
-      }
+  let querySelector = selectionArray.join(",");
+
+  for (let item of document.querySelectorAll(querySelector)) {
+    // overlay the FBC icon badge on the item
+    if (!item.classList.contains("fbc-has-badge") && !isPinterest(item) && !parentIsBadged(item)) {
+      // console.log([querySelector, item]);
+      const itemUIDClassName = "fbc-UID_" + (facebookDetectedElementsArr.length + 1);
+      const itemUIDClassTarget = "js-" + itemUIDClassName;
+      const socialAction = socialActionIntent;
+      facebookDetectedElementsArr.push(itemUIDClassName);
+      addFacebookBadge(item, itemUIDClassTarget, socialAction);
+      item.classList.add("fbc-has-badge");
+      item.classList.add(itemUIDClassName);
+      // console.log(itemUIDClassName);
     }
   }
 }
