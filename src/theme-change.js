9961edac909f05bacaf08c1633fc1b0a662ff6b2
silvererudite <shamima.hossain19@gmail.com>
2019-10-26 23:54:08 +0600

    fixed unresponsive button to match text color and hover color

--- src/theme-change.js
+++ src/theme-change.js
@@ -1,22 +1,16 @@
 
-// console.log(browser.theme.getCurrent());
-//console.log("hell");
-// var theme_color = browser.theme.getCurrent();
-// theme_color.then(function(result){
-//   console.log(result)
-// });
-//var theme_col;
 function getStyle(themeInfo) 
 {
-  if (themeInfo.colors) 
-  {
-    //theme_col=themeInfo.colors.toolbar;
-    // console.log("accent color : " +  themeInfo.colors.accentcolor);
-    // console.log("toolbar : " + themeInfo.colors.toolbar);
+  if (themeInfo.colors) {
+   
     document.body.style.backgroundColor= themeInfo.colors.toolbar;
-  document.body.style.color=themeInfo.colors.tab_background_text;
-  //console.log(themeInfo.colors);
-  }
+    document.body.style.color=themeInfo.colors.tab_background_text;
+  
+    const link_button=document.getElementsByClassName("highlight-on-hover");
+    for(var i=0;i<link_button.length;i++){
+      link_button[i].style.color =themeInfo.colors.tab_background_text;
+    }
+    }
  
 }
 
@@ -24,27 +18,10 @@ function getStyle(themeInfo)
 async function getCurrentThemeInfo() 
 {
   var themeInfo = await browser.theme.getCurrent();
-  //console.log(themeInfo);
+
   getStyle(themeInfo);
 }
 
 getCurrentThemeInfo();
-//document.body.style.backgroundColor="theme_col";
 
-// var getting = browser.theme.getCurrent();
-// console.log(getting);
 
-//console.log(theme.Theme);
-// let goodOne = Promise.resolve("Hello there");
-// //console.log(goodOne);
-// goodOne.then(function(result){
-//     console.log(result)
-// });
-
-// "content_scripts": [
-//   {
-//     "matches":["*://*.mozilla.org/*"],
-//     "js": ["check-theme.js"]
-//   }
-// ],
-//console.log("hello");
