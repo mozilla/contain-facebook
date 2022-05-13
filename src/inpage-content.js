10495eca8c80d90a11c8328ec8bc66788aadc9a6
Kaitlyn <kandres@mozilla.com>
2022-05-13 13:33:31 -0400

    Refactor content panel to iframe

---
+++ src/inpage-content.js
@@ -0,0 +1,15 @@
+
+const url = window.location.href;
+const action = url.split('?')[1].split('=')[1];
+
+const loginItem = document.getElementById("fbc-login");
+const emailItem = document.getElementById("fbc-email");
+
+
+if (action === "login") {
+    loginItem.classList.remove("is-hidden");
+}
+
+if (action === "email") {
+    emailItem.classList.remove("is-hidden");
+}
