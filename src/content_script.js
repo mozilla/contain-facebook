a2e1a5984d4c69d0a36b99b7721d218c9c1910b4
Kaitlyn <kandres@mozilla.com>
2022-05-20 11:23:42 -0700

    click outside of popup

--- src/content_script.js
+++ src/content_script.js
@@ -288,23 +288,24 @@ function createElementWithClassList(elemType, elemClass) {
 
 
 function buildInpageIframe(socialAction) {
-  const div = createElementWithClassList(
-    "div",
-    "fbc-iframe"
-  );
+  // const div = createElementWithClassList(
+  //   "div",
+  //   "fbc-iframe"
+  // );
   const iframe = document.createElement("iframe");
   iframe.src = browser.runtime.getURL(`inpage-content.html?action=${socialAction}`);
   iframe.width = 350;
   // This height is derived from the Figma file. However, this is just the starting instance of the iframe/inpage menu. After it's built out, it resizes itself based on the inner contents.
-  iframe.height = 300;
+  iframe.height = 350;
   iframe.title = browser.i18n.getMessage("facebookContainer");
   iframe.tabIndex = 0;
   iframe.ariaHidden = "false";
   iframe.id = socialAction;
+  iframe.classList.add("fbc-content-box");
 
-  div.appendChild(iframe);
+  // div.appendChild(iframe);
 
-  return div;
+  return iframe;
 }
 
 function injectIframeOntoPage(socialAction) {
@@ -314,7 +315,7 @@ function injectIframeOntoPage(socialAction) {
       "fbc-wrapper"
     );
   fbcWrapper.appendChild(fbcContent);
-  positionPrompt(fbcContent);
+  // positionPrompt(fbcContent);
   document.body.appendChild(fbcWrapper);
 
   return;
@@ -328,12 +329,27 @@ function openLoginPrompt(socialAction) {
   } else {
     hasFbcWrapper.remove();
   }
+
   // buildInpageIframe().classList.toggle("active");
   // parent.classList.toggle("active");
   // positionPrompt( htmlBadgeDiv );
   // el.classList.toggle("js-fbc-prompt-active");
   // document.body.classList.toggle("js-fbc-prompt-active");
   // htmlBadgeDiv.querySelector(".fbc-badge-prompt-btn-cancel").focus();
+}
+
+function handleIframeClosure() {
+  const iframeBox = document.querySelector(".fbc-content-box");
+
+  iframeBox.addEventListener("click", function() {
+    // e.stopPropagation();
+  });
+
+}
+
+function closeIframe() {
+  const hasFbcWrapper = document.querySelector('.fbc-wrapper');
+  hasFbcWrapper.remove();
 }
 
 function addFacebookBadge (target, badgeClassUId, socialAction) {
@@ -394,7 +410,9 @@ function addFacebookBadge (target, badgeClassUId, socialAction) {
       } 
       
       e.preventDefault();
+      e.stopPropagation();
       openLoginPrompt("login");
+      handleIframeClosure();
     });
 
     // Add to Container "Allow"
@@ -456,7 +474,8 @@ function addFacebookBadge (target, badgeClassUId, socialAction) {
         return false;
       } 
       
-      closePrompt();
+      // closePrompt();
+      closeIframe();
       const activeBadge = document.querySelector("." + badgeClassUId);
       activeBadge.style.display = "none";
     }, false);
@@ -795,17 +814,28 @@ function screenUpdate () {
 function escapeKeyListener () {
   document.body.addEventListener("keydown", function(e) {
     if(e.key === "Escape" && document.body.classList.contains("js-fbc-prompt-active")) {
-      closePrompt();
+      // closePrompt();
+      closeIframe();
     }
   });
 }
+
+window.addEventListener("click", function() {
+  if (this.document.querySelector(".fbc-wrapper")) {
+    // closeIframe();
+    console.log("check");
+  }
+  console.log("check 2");
+});
+
 
 window.addEventListener("click", function(e){
   if ( document.body.classList.contains("js-fbc-prompt-active") ) {
     const activePrompt = findActivePrompt();
     const activePromptTarget = document.querySelector(".fbc-has-badge.js-fbc-prompt-active");
     if ( !activePrompt.contains(e.target) && !activePromptTarget.contains(e.target) ) {
-      closePrompt();
+      // closePrompt();
+      closeIframe();
     }
   } else {
     contentScriptInit(true);
