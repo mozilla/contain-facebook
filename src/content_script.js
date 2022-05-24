8cfe164421259a8cdffdf73867b7bee9dab62c00
Kaitlyn <kandres@mozilla.com>
2022-05-24 11:05:48 -0700

    styling popup panel

--- src/content_script.js
+++ src/content_script.js
@@ -310,7 +310,7 @@ function buildInpageIframe(socialAction) {
   iframe.src = browser.runtime.getURL(`inpage-content.html?action=${socialAction}`);
   iframe.width = 350;
   // This height is derived from the Figma file. However, this is just the starting instance of the iframe/inpage menu. After it's built out, it resizes itself based on the inner contents.
-  iframe.height = 350;
+  iframe.height = 250;
   iframe.title = browser.i18n.getMessage("facebookContainer");
   iframe.tabIndex = 0;
   iframe.ariaHidden = "false";
