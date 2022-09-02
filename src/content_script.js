97fad5789913f9aa45a27f3705f30c3518dafd8d
Kaitlyn <kandres@mozilla.com>
2022-09-02 12:06:10 -0400

    breakdown positioniframe into functions

--- src/content_script.js
+++ src/content_script.js
@@ -269,55 +269,64 @@ function positionIframe(fencePos) {
 
   // Desktop Orientation
   if (iconRightAllowance > iframePaddingAllowance || iconLeftAllowance > iframePaddingAllowance) {
-
-    // Position iframe relative to FBC Icon
-    iframeBox.style.marginLeft = `${xRight}px`;
-    iframeBox.style.marginTop = `${yPos}px`;
-
-    // Add Chevron (Default left arrow)
-    const xPosChevron = xRight - iframeChevron.offsetWidth;
-    const yPosChevron = yPos + offsetY;
-
-    iframeChevron.style.marginLeft = `${xPosChevron}px`;
-    iframeChevron.style.marginTop = `${yPosChevron}px`;
-
-    const calculateOffsetDiff = window.innerWidth - fencePosition.x;
-
-    // Flip the iframe to show on the left side when icon is too close to the edge
-    if (iframePaddingAllowance > calculateOffsetDiff) {
-      iframeBox.style.marginLeft = `${xLeft}px`;
-      iframeChevron.classList.add("fbc-chevron-arrow-right");
-      iframeChevron.style.marginLeft = `${xPosChevron - fencePos.offsetWidth - iframeChevron.offsetWidth - offsetX}px`;
-    }
-
-    else {
-      iframeChevron.classList.remove("fbc-chevron-arrow-right");
-    }
-
-    iframeChevron.classList.remove("fbc-chevron-arrow-top");
+    desktopOrientation(iframeBox, xRight, yPos, xLeft, iframeChevron, offsetY, fencePosition, iframePaddingAllowance, fencePos, offsetX);
   }
 
   // Mobile Orientation
   else {
-    for (const panels of iframeElement) {
-      panels.width = window.innerWidth;
-      if (window.innerWidth > 480) {
-        panels.width = 350;
-      }
-    }
-
-    iframeChevron.classList.add("fbc-chevron-arrow-top");
-    iframeBox.style.marginTop = `${yPosMobile}px`;
-
-    const xPosChevronMobile = xPosMobile;
-    const yPosChevronMobile = yPosMobile - iframeChevron.offsetWidth;
-
-    iframeChevron.style.marginLeft = `${xPosChevronMobile}px`;
-    iframeChevron.style.marginTop = `${yPosChevronMobile}px`;
-
+    mobileOrientation(iframeElement, iframeChevron, iframeBox, yPosMobile, xPosMobile);
   }
 
 }
+
+function mobileOrientation(iframeElement, iframeChevron, iframeBox, yPosMobile, xPosMobile){
+  for (const panels of iframeElement) {
+    panels.width = window.innerWidth;
+    if (window.innerWidth > 480) {
+      panels.width = 350;
+    }
+  }
+
+  iframeChevron.classList.add("fbc-chevron-arrow-top");
+  iframeBox.style.marginTop = `${yPosMobile}px`;
+
+  const xPosChevronMobile = xPosMobile;
+  const yPosChevronMobile = yPosMobile - iframeChevron.offsetWidth;
+
+  iframeChevron.style.marginLeft = `${xPosChevronMobile}px`;
+  iframeChevron.style.marginTop = `${yPosChevronMobile}px`;
+
+}
+
+function desktopOrientation(iframeBox, xRight, yPos, xLeft, iframeChevron, offsetY, fencePosition, iframePaddingAllowance, fencePos, offsetX) {
+
+  // Position iframe relative to FBC Icon
+  iframeBox.style.marginLeft = `${xRight}px`;
+  iframeBox.style.marginTop = `${yPos}px`;
+
+  // Add Chevron (Default left arrow)
+  const xPosChevron = xRight - iframeChevron.offsetWidth;
+  const yPosChevron = yPos + offsetY;
+
+  iframeChevron.style.marginLeft = `${xPosChevron}px`;
+  iframeChevron.style.marginTop = `${yPosChevron}px`;
+
+  const calculateOffsetDiff = window.innerWidth - fencePosition.x;
+
+  // Flip the iframe to show on the left side when icon is too close to the edge
+  if (iframePaddingAllowance > calculateOffsetDiff) {
+    iframeBox.style.marginLeft = `${xLeft}px`;
+    iframeChevron.classList.add("fbc-chevron-arrow-right");
+    iframeChevron.style.marginLeft = `${xPosChevron - fencePos.offsetWidth - iframeChevron.offsetWidth - offsetX}px`;
+  }
+
+  else {
+    iframeChevron.classList.remove("fbc-chevron-arrow-right");
+  }
+
+  iframeChevron.classList.remove("fbc-chevron-arrow-top");
+}
+
 
 
 function openInputPrompt(socialAction, fencePos, target, fbcIframeHeight) {
